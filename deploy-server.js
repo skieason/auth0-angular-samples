52a34cfcba1ae63216d319d3b696dd641da5c35c
Phil Varner <philvarnerlp9@gmail.com>
2022-03-31 07:09:53 -1000

    join path with root in node

--- deploy-server.js
+++ deploy-server.js
@@ -7,13 +7,19 @@
 // }
 
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
 // app.use(requireHTTPS);
 app.use(express.static('dist/login-demo'));
 
-app.get('/', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/login-demo/'}),
-);
+// app.get('/', (req, res) =>
+//     res.sendFile('index.html', {root: 'dist/login-demo/'}),
+// );
+
+app.get('*',(req,res) =>{
+    res.sendFile(path.join(__dirname,'dist/login-demo/index.html'));
+});
 
 app.listen(process.env.PORT || 8080, () => console.log(`Api started..`));
